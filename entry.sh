0a682e422017a3e247f6029cb3578078f491167c
Joe Yates <joe.g.yates@gmail.com>
2023-10-09 12:24:55 +0200

    Don't change ownership of the images directory

--- entry.sh
+++ entry.sh
@@ -2,6 +2,4 @@
 
 set -euxo pipefail
 
-sudo chown -R imageflow /home/imageflow/images
-
 /home/imageflow/imageflow_server start --bind-address 0.0.0.0 --port 3000 --data-dir /home/imageflow/data/ --mount /images/:ir4_local:/home/imageflow/images/
